74734c64296f03999cc6c90b9cab132efb12cc72
aj132608 <32584157+aj132608@users.noreply.github.com>
2020-03-02 14:58:43 -0600

    Added comments with explainations.

--- request_test.py
+++ request_test.py
@@ -1,17 +1,22 @@
 import requests
 
 if __name__ == "__main__":
+    # Construct the URL and message to send to the microservice
     url = 'http://127.0.0.1:8080/post-example'
     json_message = {
         "message": "hello",
         "author": "Alex Jirovsky"
     }
 
+    # Using the requests library, send a POST request with the following
+    # message to the URL.
     response = requests.post(url=url,
                              json=json_message)
 
+    # Extract the status code and returned text
     status_code = response.status_code
     response_body = response.text
 
+    # Print out the responses
     print(f"status code: {status_code}")
     print(f"response: {response_body}")
